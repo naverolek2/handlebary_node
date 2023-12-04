bc2203269e85c89420725663142e22a437f5bc92
Naverolek2 <sebaskiba11@gmail.com>
2023-12-04 09:37:44 +0100

    eesssa

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('views', './src/views');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    //res.send("Stronka działa!");
+    res.render('index', {
+        pageTitle: 'Strona główna'
+    })
+});
+app.get('/omnie', (req, res) => {
+    res.render('omnie', {
+        pageTitle: 'Strona o mnie'
+    })
+});
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt', {
+        pageTitle: 'Strona kontakt'
+
+    })
+});
+
+
+
+app.listen(5000, ()=> {
+    console.log("Stronka działa");
+});
